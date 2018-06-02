aba2e7a3df61fb78c37110897d297ec831508fe8
Alexey Subbotin <subbotin910@gmail.com>
2018-06-02 11:52:26 +0500

    each continent count input

--- Subb_Lab_15/Subb_Lab_15/Program.cs
+++ Subb_Lab_15/Subb_Lab_15/Program.cs
@@ -10,6 +10,30 @@ namespace Subb_Lab_15
     {
         static void Main(string[] args)
         {
+            // Earth list.
+            List<List<State>> earth = new List<List<State>>();
+
+            int africaCount = CountInput("Enter the number of African countries: ");
+            int asiaCount = CountInput("Enter the number of Asian countries: ");
+            int americaCount = CountInput("Enter the number of American countries: ");
+            int oceaniaCount = CountInput("Enter the number of Oceanian countries: ");
+            int europeCount = CountInput("Enter the number of European countries: ");
+
+        }
+
+        public static int CountInput(string s)
+        {
+            bool ok;
+            int n;
+            do
+            {
+                Console.WriteLine(s);
+                ok = Int32.TryParse(Console.ReadLine(), out n);
+                if (!ok || n < 0)
+                    Console.WriteLine("Input error! Perhaps you didn't enter a natural number");
+            } while (!ok || n < 0);
+
+            return n;
         }
     }
 }
